9eb50d9f39f70f997404d213182ff0de775a31ab
enes <enesselcu25@gmail.com>
2022-08-06 11:02:00 +0300

    item_view is removed

--- app/src/main/java/com/enesselcuk/jatpeackdatastore/util/PreferencesKey.kt
+++ app/src/main/java/com/enesselcuk/jatpeackdatastore/util/PreferencesKey.kt
@@ -6,6 +6,4 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 object PreferencesKey {
     val SHOW_COMPLETED = booleanPreferencesKey("show_completed")
     val SORT_ORDER = stringPreferencesKey("sort_order")
-
-
 }
